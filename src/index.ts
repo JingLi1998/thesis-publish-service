44ee8615083df33402db0f0735098a254cb07aff
Jing Li <jing_li1998@hotmail.com>
2020-09-19 16:09:09 +1000

    add process.env config for heroku

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 
 const app = express();
-const PORT = 8000;
 
 app.get("/", (_req, res) => res.send("Express + Typescript Server"));
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(
+    `[server]: Server is running at http://localhost:${
+      process.env.PORT || 8000
+    }`
+  );
 });
