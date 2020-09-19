8488f2a940d1e17a0e0c7236ce2a4d1afa7b18cd
Jing Li <jing_li1998@hotmail.com>
2020-09-19 16:50:38 +1000

    add google drive module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import { google } from "googleapis";
+
+dotenv.config();
+
+const drive = google.drive({
+  version: "v3",
+  auth: process.env.GOOGLE_API_KEY,
+});
+
+drive.about.get();
 
 const app = express();
 
-app.get("/", (_req, res) => res.send("Express + Typescript Server"));
+app.get(`/`, (_req, res) => res.send(`Express + Typescript Server`));
+
+app.get(`/watch`, async (_req, res) => {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/drive/v3/${process.env.GOOGLE_SHEET_ID}`
+    );
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(
